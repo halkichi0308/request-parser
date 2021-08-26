314d510a7438d5a35114da0c52eee6ce79b1ef98
halkichi0308 <m.kouda0308@gmail.com>
2021-08-26 23:02:41 +0900

    Refactor variable name, and fix lint.

--- src/main/java/burp/com/burp/assets/DefaultTemplate.java
+++ src/main/java/burp/com/burp/assets/DefaultTemplate.java
@@ -6,25 +6,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class MemoTemplate {
-    private static String memorandom_format = "[Format]\n" +
-    "[Target URL]: " + "\n" + 
-    "§§pentest_target§§" + "\n\n" + 
-    "[Referer]: " + "\n" + 
-    "§§referred_from§§"  + "\n\n" + 
-    "[Method]: "  + "\n" + 
-    "§§method§§" + "\n\n" + 
-    "[Cookie]: " + "\n" + 
-    "§§cookies§§" + "\n\n" + 
-    "[GET Parametor]: " + "\n" + 
-    "§§get_params§§" + "\n\n" + 
-    "[POST Parametor]: " + "\n" + 
-    "§§body_params§§" + "\n\n" + 
-    "[Total Parametor(without Cookies)]: " + "\n" + 
-    "§§params_count§§";
+public class DefaultTemplate {
+    private static String memorandom_format = "[Format]\n" + "[Target URL]: " + "\n" + "§§pentest_target§§" + "\n\n"
+            + "[Referer]: " + "\n" + "§§referred_from§§" + "\n\n" + "[Method]: " + "\n" + "§§method§§" + "\n\n"
+            + "[Cookie]: " + "\n" + "§§cookies§§" + "\n\n" + "[GET Parametor]: " + "\n" + "§§get_params§§" + "\n\n"
+            + "[POST Parametor]: " + "\n" + "§§body_params§§" + "\n\n" + "[Total Parametor(without Cookies)]: " + "\n"
+            + "§§params_count§§";
 
     /**
-     * Read 'template.txt', if the file is exist.
+     * Burp will load 'template.txt', if the file is exist in same directory.
      * 
      * @throws IOException
      * 
@@ -33,7 +23,7 @@ public class MemoTemplate {
         Path path = Paths.get("./template.txt");
         StringBuffer template = new StringBuffer();
         try (Stream<String> lines = Files.lines(path)) {
-            if(Files.size(path) > 1024 * 10240){
+            if (Files.size(path) > 1024 * 10240) {
                 throw new IOException("[Info]: File size is too large.");
             }
             lines.forEach(lineChar -> template.append(lineChar + "\n"));
